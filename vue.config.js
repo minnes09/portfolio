89aa2a29dfb222972c565481f846162eee402fe6
minnes09 <gabriele.minneci@gmail.com>
2025-10-04 16:20:23 +0200

    changed env path

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   // Change this if you deploy to a different repo/name
-  publicPath: process.env.VUE_APP_PUBLIC_PATH || '/'
+  publicPath: process.env.VUE_APP_PUBLIC_PATH || '/portfolio/'
 };
